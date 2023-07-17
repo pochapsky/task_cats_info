41e2b4a4c59904ecc0df02be6fe03ddf6c40ddeb
Konstantin Pochapsky <chernoknignikvladikus@yandex.ru>
2023-07-17 10:59:07 +0300

    Initial commit

---
+++ src/main/java/org/example/Cats.java
@@ -0,0 +1,58 @@
+package org.example;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cats {
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final Integer upvotes;
+
+    public Cats(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") Integer upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cats: " +
+                "\n id='" + id +
+                "\n text='" + text +
+                "\n type='" + type +
+                "\n user='" + user +
+                "\n upvotes=" + upvotes;
+
+    }
+}
+
